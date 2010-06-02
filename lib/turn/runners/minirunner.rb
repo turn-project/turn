47df21e27af1c16f1bf2b8da96c7533c88528dbb
7rans <transfire@gmail.com>
2010-06-02 09:59:08 -0400

    cleanup minitest runner code

--- lib/turn/runners/minirunner.rb
+++ lib/turn/runners/minirunner.rb
@@ -16,6 +16,12 @@ module Turn
 
       [controller.files].flatten.each{ |path| require(path) }
 
+      files = [controller.files].flatten
+      files.each{ |path| require(path) }   
+
+      # TODO: Better name ?
+      @turn_suite_name = files.map{ |path| File.dirname(path).sub(Dir.pwd+'/','') }.uniq.join(',')
+
       #sub_suites = []
       #ObjectSpace.each_object(Class) do |klass|
       #  if(Test::Unit::TestCase > klass)
@@ -59,65 +65,37 @@ module Turn
 
       run_test_suites(filter)
 
-      #@@out.puts
-      #@@out.puts "Finished in #{'%.6f' % (Time.now - start)} seconds."
-
-      #@@out.puts
-
-      #@@out.print "%d tests, " % test_count
-      #@@out.print "%d assertions, " % assertion_count
-      #@@out.print red { "%d failures, " % failures }
-      #@@out.print yellow { "%d errors, " % errors }
-      #@@out.puts cyan { "%d skips" % skips}
-
       return failures + errors if @test_count > 0 # or return nil...
     end
 
-    # NOTES: MiniTest somehow manages to confuse a suite for a case, and a case for a unit test.
+    #
     def run_test_suites(filter = /./)
       @test_count, @assertion_count = 0, 0
       old_sync, @@out.sync = @@out.sync, true if @@out.respond_to? :sync=
 
-      @turn_suite = Turn::TestSuite.new #@suite
+      @turn_suite = Turn::TestSuite.new(@turn_suite_name)
       @turn_suite.size = ::MiniTest::Unit::TestCase.test_suites.size
       @turn_logger.start_suite(@turn_suite)
 
-      ::MiniTest::Unit::TestCase.test_suites.each do |suite|
-        test_cases = suite.test_methods.grep(filter)
+      ::MiniTest::Unit::TestCase.test_suites.each do |kase|
 
-        @turn_case = @turn_suite.new_case(suite.name)
+        test_cases = kase.test_methods.grep(filter)
+
+        @turn_case = @turn_suite.new_case(kase.name)
+
+        turn_cases = test_cases.map do |test|
+          @turn_case.new_test(test)
+        end
+
         @turn_logger.start_case(@turn_case)
 
-        #if test_cases.size > 0
-        #  @@out.print "\n#{suite}:\n"
-        #end
-
-        test_cases.each do |test|
+        turn_cases.each do |test|
           #methname, tcase = name.scan(%r/^([^\(]+)\(([^\)]+)\)/o).flatten!
-          @turn_test = @turn_case.new_test(test)
+          @turn_test = test #@turn_case.new_test(test)
           @turn_logger.start_test(@turn_test)
 
-          inst = suite.new(test)
+          inst = kase.new(test.name)
           inst._assertions = 0
-
-          #t = Time.now
-
-          #@broken = nil
-
-          #@@out.print(case inst.run(self)
-          #            when :pass
-          #              @broken = false
-          #              green { pad_with_size "PASS" }
-          #            when :error
-          #              @broken = true
-          #              yellow { pad_with_size "ERROR" }
-          #            when :fail
-          #              @broken = true
-          #              red { pad_with_size "FAIL" }
-          #            when :skip
-          #              @broken = false
-          #              cyan { pad_with_size "SKIP" }
-          #            end)
 
           result = inst.run(self)
           report = @report.last
@@ -138,19 +116,6 @@ module Turn
             @turn_logger.skip #(report)
           end
 
-          #@@out.print " #{test}"
-          #@@out.print " (%.2fs) " % (Time.now - t)
-
-          #if @broken
-            #@@out.puts
-            #report = @report.last
-            #@@out.puts pad(report[:message], 10)
-            #trace = MiniTest::filter_backtrace(report[:exception].backtrace).first
-            #@@out.print pad(trace, 10)
-
-          #  @@out.puts
-          #end
-
           @turn_logger.finish_test(@turn_test)
 
           @test_count += 1
@@ -162,14 +127,6 @@ module Turn
       @@out.sync = old_sync if @@out.respond_to? :sync=
       [@test_count, @assertion_count]
     end
-
-    #def pad(str, size=PADDING_SIZE)
-    #  " " * size + str
-    #end
-
-    #def pad_with_size(str)
-    #  pad("%5s" % str)
-    #end
 
     # Overwrite #puke method so that is stores a hash
     # with :message and :exception keys.
@@ -206,16 +163,16 @@ class ::MiniTest::Unit::TestCase
     begin
       @passed = nil
       self.setup
-      self.__send__(self.__name__)
+      self.__send__(self.__name__.to_s)
       @passed = true
     rescue Exception => e
       @passed = false
-      result = runner.puke(self.class, self.__name__, e)
+      result = runner.puke(self.class, self.__name__.to_s, e)
     ensure
       begin
         self.teardown
       rescue Exception => e
-        result = runner.puke(self.class, self.__name__, e)
+        result = runner.puke(self.class, self.__name__.to_s, e)
       end
     end
     result
