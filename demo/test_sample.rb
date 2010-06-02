39ac78a513160e90a41490432548b22835246a13
7rans <transfire@gmail.com>
2010-06-02 10:04:58 -0400

    add demo for visible inspections

---
+++ demo/test_sample.rb
@@ -0,0 +1,19 @@
+class SampleTest < Test::Unit::TestCase
+
+  def test_sample_pass
+    assert_equal(1,1)
+  end
+
+  def test_sample_fail
+    assert_equal(1,2)
+  end
+
+  def test_sample_error
+    raise StandardError, "Sample of a raised exception."
+  end
+
+end
+
+
+class EmptyTest < Test::Unit::TestCase
+end
