caf2cc74dc412360be0d068225a725efcc1cf5f5
7rans <transfire@gmail.com>
2010-09-07 18:42:22 -0400

    add extra demo test for trying out solo and cross runners

---
+++ demo/test_sample2.rb
@@ -0,0 +1,33 @@
+class Sample2Case1 < Test::Unit::TestCase
+  def test_sample_pass1
+    assert_equal(1,1)
+  end
+  def test_sample_pass2
+    assert_equal(2,2)
+  end
+
+  def test_sample_fail1
+    assert_equal(1,2)
+  end
+  def test_sample_fail2
+    assert_include(1,[])
+  end
+
+  def test_sample_error1
+    raise StandardError, "Raised exception!"
+  end
+  def test_sample_error2
+    raise StandardError, "Raised another exception!"
+  end
+end
+
+
+class Sample2Case2 < Test::Unit::TestCase
+  def test_sample_pass1
+    assert_equal(1,1)
+  end
+  def test_sample_pass2
+    assert_equal(1,1)
+  end
+end
+
