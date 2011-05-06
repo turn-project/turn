442e66c1db7b2a2ada5111edb520dfc0e8dc8700
yalab <rudeboyjet@gmail.com>
2011-05-06 21:05:28 +0900

    Add -t or --trace option for display all stacktrace.

--- test/helper.rb
+++ test/helper.rb
@@ -4,6 +4,7 @@ if RUBY_VERSION < "1.9"
   require 'test/unit'
 else
   require 'minitest/unit'
+  require 'test/unit'
 end
 
 #
@@ -95,3 +96,18 @@ end
   save_test(text, 'minitest_autorun_test.rb')
 end
 
+
+def setup_minitest_autorun_with_fail
+  text = <<-HERE
+require 'minitest/unit'
+require 'rubygems'
+require 'turn'
+MiniTest::Unit.autorun
+class TestTest < MiniTest::Unit::TestCase
+  def test_sample_pass
+    assert_equal(0,1)
+  end
+end
+  HERE
+  save_test(text, 'minitest_autorun_test_with_fail.rb')
+end
