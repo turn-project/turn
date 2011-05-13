b5fed3a87450912680354069166712c20be88d5e
7rans <transfire@gmail.com>
2011-05-13 12:49:33 -0400

    missing #skip! method in TestMethod

--- lib/turn/components/method.rb
+++ lib/turn/components/method.rb
@@ -12,27 +12,38 @@ module Turn
       @name      = name
       @fail      = false
       @error     = false
+      @skip      = false
       @raised    = nil
       @message   = nil
       @backtrace = []
     end
 
     def fail!(assertion)
-      @fail, @error = true, false
+      @fail, @error, @skip = true, false, false
       @raised    = assertion
       @message   = assertion.message
       @backtrace = assertion.backtrace
     end
 
     def error!(exception)
-      @fail, @error = false, true
+      @fail, @error, @skip = false, true, false
       @raised    = exception
       @message   = exception.message
       @backtrace = exception.backtrace
     end
 
+    def skip!(assertion)
+      @fail, @error, @skip = false, false, true
+      @raised    = assertion
+      @message   = assertion.message
+      @backtrace = assertion.backtrace
+    end
+
     def fail?  ; @fail  ; end
     def error? ; @error ; end
+    def skip?  ; @skip  ; end
+
+    # TODO: should this include `or @skip`?
     def pass?  ; !(@fail or @error) ; end
 
     def to_s ; name ; end
