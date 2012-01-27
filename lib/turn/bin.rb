8239e27d464f404b50a993dafc1ac7e66ed4d664
7rans <transfire@gmail.com>
2012-01-27 12:35:26 -0500

    Add error catch around executable for nicer error messages.

--- lib/turn/bin.rb
+++ lib/turn/bin.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.unshift(File.dirname(File.dirname(__FILE__)))
 require 'turn/command'
-Turn::Command.main(*ARGV)
+#begin
+  Turn::Command.main(*ARGV)
+#rescue StandardError => e
+#  raise if $DEBUG
+#  puts e
+#  exit -1
+#end
 
