9e0c2af6d33045f4beb72164cb00a4e4fff4e7aa
7rans <transfire@gmail.com>
2012-02-10 16:56:10 -0500

    Fix test_runners for new default of pretty reporter.

--- test/test_runners.rb
+++ test/test_runners.rb
@@ -5,17 +5,17 @@ class TestRunners < Test::Unit::TestCase
   def test_solo
     file = setup_testunit(false, 'test_solo.rb')
     result = turn2 '--solo', file
-    assert result.index('pass: 1'),  "ACTUAL RESULT --->\n #{result}"
-    assert result.index('fail: 0'),  "ACTUAL RESULT --->\n #{result}"
-    assert result.index('error: 0'), "ACTUAL RESULT --->\n #{result}"
+    assert result.index('1 passed'),  "ACTUAL RESULT:\n #{result}"
+    assert result.index('0 failures'),  "ACTUAL RESULT:\n #{result}"
+    assert result.index('0 errors'), "ACTUAL RESULT:\n #{result}"
   end
 
   def test_cross
     file1 = setup_testunit(false, 'test1.rb')
     file2 = setup_testunit(false, 'test2.rb')
     result = turn2 '--cross', file1, file2
-    assert result.index('pass: 2'),  "ACTUAL RESULT --->\n #{result}"
-    assert result.index('error: 0'), "ACTUAL RESULT --->\n #{result}"
+    assert result.index('2 passed'),  "ACTUAL RESULT:\n #{result}"
+    assert result.index('0 errors'), "ACTUAL RESULT:\n #{result}"
   end
 
   # autorun
@@ -25,9 +25,9 @@ class TestRunners < Test::Unit::TestCase
     def test_autorun_testunit
       file = setup_testunit('turn/autorun', 'test_autorun.rb')
       result = `ruby -Ilib #{file} 2>&1`
-      assert(result.index('pass: 1'),  "ACTUAL RESULT:\n #{result}")
-      assert(result.index('fail: 0'),  "ACTUAL RESULT:\n #{result}")
-      assert(result.index('error: 0'), "ACTUAL RESULT:\n #{result}")
+      assert(result.index('1 passed'),  "ACTUAL RESULT:\n #{result}")
+      assert(result.index('0 failures'),  "ACTUAL RESULT:\n #{result}")
+      assert(result.index('0 errors'), "ACTUAL RESULT:\n #{result}")
     end
 
   #else
@@ -35,16 +35,16 @@ class TestRunners < Test::Unit::TestCase
     def test_autorun
       file = setup_minitest_autorun
       result = `ruby -Ilib #{file} 2>&1`
-      assert result.index('fail: 0'),  "ACTUAL RESULT --->\n #{result}"
-      assert result.index('error: 0'), "ACTUAL RESULT --->\n #{result}"
+      assert result.index('0 failures'),  "ACTUAL RESULT:\n #{result}"
+      assert result.index('0 errors'), "ACTUAL RESULT:\n #{result}"
     end
 
     def test_autorun_with_trace
       file = setup_minitest_autorun_with_trace
 
       result = `ruby -Ilib #{file} 2>&1`
-      assert result.index('fail: 1'), 'fail is not 1'
-      assert result.index('error: 0'), 'error is not 0'
+      assert result.index('1 failures'), 'fail is not 1'
+      assert result.index('0 errors'), 'error is not 0'
 
       # TODO: the backtrace is empty, why?
       #assert result.scan(/\.rb:\d+:in/).length > 1
